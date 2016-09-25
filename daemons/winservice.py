b990738aca3f22b99f70e3e5f320f7846029aada
kasramvd <kasraavand@gmail.com>
2016-09-26 01:20:44 +0330

    update winservice

--- daemons/winservice.py
+++ daemons/winservice.py
@@ -67,7 +67,7 @@ class Service(win32serviceutil.ServiceFramework):
         try:
             self.ReportServiceStatus(win32service.SERVICE_RUNNING)
             self.log('start')
-            self.start()
+            self.executable()
             self.log('wait')
             win32event.WaitForSingleObject(self.stop_event, win32event.INFINITE)
             self.log('done')
@@ -75,82 +75,18 @@ class Service(win32serviceutil.ServiceFramework):
             self.log('Exception : {}'.format(exc))
             self.stop()
 
-    def stop(self):
-        """
-        .. py:attribute:: SvcStop()
-
-        Stop the service.
-
-           :rtype: None
-
-        """
+    def SvcStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        self.log('stopping')
-        self.stop()
         self.log('stopped')
         win32event.SetEvent(self.stop_event)
         self.ReportServiceStatus(win32service.SERVICE_STOPPED)
 
-    def start(self, executable):
-        """
-        .. py:attribute:: start()
 
-        Run the executable.
-
-           :param executable: The executable object.
-           :type executable: Callable
-           :rtype: None
-
-        """
-        self.log("Start service!")
-        executable()
+def stop(self):
+    win32serviceutil.StopService(Service)
+    print('Service stopped...')
 
 
-def instart(cls, name, display_name=None, stay_alive=True):
-    """
-    ==============
-
-    ``instart``
-    ----------
-
-    .. py:function:: instart(['cls', 'name', 'display_name', 'stay_alive'])
-
-    Install and  Start (auto) a Service
-
-       :param cls: the class (derived from Service) that implement the Service
-       :type cls: Class object
-       :param name: Service name
-       :type name: str
-       :param display_name: the name displayed in the service manager
-       :type display_name: str
-       :param stay_alive: Service will stop on logout if False
-       :type stay_alive: boolean
-       :rtype: None
-
-    """
-    cls._svc_name_ = name
-    cls._svc_display_name_ = display_name or name
-    try:
-        module_path = modules[cls.__module__].__file__
-    except AttributeError:
-        # maybe py2exe went by
-        from sys import executable
-        module_path = executable
-    module_file = splitext(abspath(module_path))[0]
-    cls._svc_reg_class_ = '{}.{}'.format(module_file, cls.__name__)
-    if stay_alive:
-        win32api.SetConsoleCtrlHandler(lambda x: True, True)
-    try:
-        win32serviceutil.InstallService(
-            cls._svc_reg_class_,
-            cls._svc_name_,
-            cls._svc_display_name_,
-            startType=win32service.SERVICE_AUTO_START
-        )
-        print('Install ok')
-        win32serviceutil.StartService(
-            cls._svc_name_
-        )
-        print('Start ok')
-    except Exception, x:
-        print str(x)
+def start(self, executable):
+    Service.executable = executable
+    win32serviceutil.StartService(Service)

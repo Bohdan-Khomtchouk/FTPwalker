231e7bc66b5873e3d11802d999ee0fe78c51d6b4
kasramvd <kasraavand@gmail.com>
2016-09-28 05:11:24 +0330

    Change the structure and refine some minor bugs

--- daemons/winservice.py
+++ daemons/winservice.py
@@ -4,14 +4,10 @@ winservice.py
 =====
 
 Daemon service for windows.
-Source: http://code.activestate.com/recipes/551780/
 
 ============================
 
 """
-
-from os.path import splitext, abspath
-from sys import modules
 
 import win32serviceutil
 import win32service
@@ -20,60 +16,33 @@ import win32api
 
 
 class Service(win32serviceutil.ServiceFramework):
-    """
-    ==============
-
-    ``Service``
-    ----------
-
-    .. py:class:: Service()
-
-    """
     _svc_name_ = '_unNamed'
     _svc_display_name_ = '_Service Template'
 
     def __init__(self, *args):
-        """
-        .. py:attribute:: __init__()
-
-           :rtype: None
-        """
         win32serviceutil.ServiceFramework.__init__(self, *args)
         self.log('init')
         self.stop_event = win32event.CreateEvent(None, 0, 0, None)
 
     def log(self, msg):
-        """
-        .. py:attribute:: log()
-
-           :param msg: log message.
-           :type msg: str
-           :rtype: None
-
-        """
         import servicemanager
         servicemanager.LogInfoMsg(str(msg))
 
+    def sleep(self, sec):
+        win32api.Sleep(sec * 1000, True)
+
     def SvcDoRun(self):
-        """
-        .. py:attribute:: SvcDoRun()
-
-        Running the service.
-
-           :rtype: None
-
-        """
         self.ReportServiceStatus(win32service.SERVICE_START_PENDING)
         try:
             self.ReportServiceStatus(win32service.SERVICE_RUNNING)
             self.log('start')
-            self.executable()
+            self.start()
             self.log('wait')
             win32event.WaitForSingleObject(self.stop_event, win32event.INFINITE)
             self.log('done')
-        except Exception as exc:
-            self.log('Exception : {}'.format(exc))
-            self.stop()
+        except Exception as x:
+            self.log('Exception : %s' % x)
+            self.SvcStop()
 
     def SvcStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
@@ -81,12 +50,46 @@ class Service(win32serviceutil.ServiceFramework):
         win32event.SetEvent(self.stop_event)
         self.ReportServiceStatus(win32service.SERVICE_STOPPED)
 
+    def start(self):
+        self.executable()
 
-def stop(self):
-    win32serviceutil.StopService(Service)
-    print('Service stopped...')
+    def stop(self):
+        self.SvcStop()
 
 
-def start(self, executable):
-    Service.executable = executable
-    win32serviceutil.StartService(Service)
+class Daemon:
+    def __init__(self, stay_alive=True, **kwargs):
+        self.name = kwargs['name']
+        self.display_name = kwargs['display_name']
+        self.stay_alive = stay_alive
+
+    def start(self, executable):
+
+        try:
+            setattr(Service, 'executable', executable)
+            setattr(Service, '_svc_name_', self.name)
+            setattr(Service, '_svc_display_name_', self.display_name or self.name)
+            setattr(Service, '_svc_reg_class_', "Service")
+        except:
+            raise
+
+        if self.stay_alive:
+            win32api.SetConsoleCtrlHandler(lambda x: True, True)
+        try:
+            win32serviceutil.InstallService(
+                Service._svc_reg_class_,
+                Service._svc_name_,
+                Service._svc_display_name_,
+                startType=win32service.SERVICE_AUTO_START
+            )
+            print('Install ok')
+            win32serviceutil.StartService(
+                Service._svc_name_
+            )
+            print('Start ok')
+        except Exception as x:
+            print(str(x))
+
+    def stop(self):
+        win32serviceutil.StopService(self.name)
+        # win32serviceutil.ServiceCtrlHandler(win32service.SERVICE_CONTROL_STOP)

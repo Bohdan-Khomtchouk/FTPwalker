56f269aac946907b9dfdb617281cb246f9ccb541
kasramvd <kasraavand@gmail.com>
2016-10-06 21:34:16 +0330

    add instart function

--- daemons/winservice.py
+++ daemons/winservice.py
@@ -9,28 +9,27 @@ Daemon service for windows.
 
 """
 
+from os.path import splitext, abspath
+from sys import modules
 import win32serviceutil
 import win32service
 import win32event
 import win32api
+import servicemanager
 
 
 class Service(win32serviceutil.ServiceFramework):
     _svc_name_ = '_unNamed'
     _svc_display_name_ = '_Service Template'
-
     def __init__(self, *args):
         win32serviceutil.ServiceFramework.__init__(self, *args)
         self.log('init')
         self.stop_event = win32event.CreateEvent(None, 0, 0, None)
-
     def log(self, msg):
         import servicemanager
         servicemanager.LogInfoMsg(str(msg))
-
     def sleep(self, sec):
-        win32api.Sleep(sec * 1000, True)
-
+        win32api.Sleep(sec*1000, True)
     def SvcDoRun(self):
         self.ReportServiceStatus(win32service.SERVICE_START_PENDING)
         try:
@@ -43,53 +42,56 @@ class Service(win32serviceutil.ServiceFramework):
         except Exception as x:
             self.log('Exception : %s' % x)
             self.SvcStop()
-
     def SvcStop(self):
         self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        self.log('stopping')
+        self.stop()
         self.log('stopped')
         win32event.SetEvent(self.stop_event)
         self.ReportServiceStatus(win32service.SERVICE_STOPPED)
-
+    # to be overridden
     def start(self):
-        self.executable()
-
+        pass
+    # to be overridden
     def stop(self):
-        self.SvcStop()
+        pass
+
+def instart(cls, name, display_name=None, stay_alive=True):
+    ''' Install and  Start (auto) a Service
+        cls : the class (derived from Service) that implement the Service
+        name : Service name
+        display_name : the name displayed in the service manager
+        stay_alive : Service will stop on logout if False
+    '''
+    cls._svc_name_ = name
+    cls._svc_display_name_ = display_name or name
+    try:
+        module_path = modules[cls.__module__].__file__
+    except AttributeError:
+        # maybe py2exe went by
+        from sys import executable
+        module_path = executable
+    module_file = splitext(abspath(module_path))[0]
+    cls._svc_reg_class_ = '%s.%s' % (module_file, cls.__name__)
+    if stay_alive:
+        win32api.SetConsoleCtrlHandler(lambda x: True, True)
+    try:
+        win32serviceutil.InstallService(
+            cls._svc_reg_class_,
+            cls._svc_name_,
+            cls._svc_display_name_,
+            startType=win32service.SERVICE_AUTO_START
+        )
+        print('Install ok')
+        win32serviceutil.StartService(
+            cls._svc_name_
+        )
+        print('Start ok')
+    except Exception as x:
+        print(str(x))
 
 
-class Daemon:
-    def __init__(self, stay_alive=True, **kwargs):
-        self.name = kwargs['name']
-        self.display_name = kwargs['display_name']
-        self.stay_alive = stay_alive
-
-    def start(self, executable):
-
-        try:
-            setattr(Service, 'executable', executable)
-            setattr(Service, '_svc_name_', self.name)
-            setattr(Service, '_svc_display_name_', self.display_name or self.name)
-            setattr(Service, '_svc_reg_class_', "Service")
-        except:
-            raise
-
-        if self.stay_alive:
-            win32api.SetConsoleCtrlHandler(lambda x: True, True)
-        try:
-            win32serviceutil.InstallService(
-                Service._svc_reg_class_,
-                Service._svc_name_,
-                Service._svc_display_name_,
-                startType=win32service.SERVICE_AUTO_START
-            )
-            print('Install ok')
-            win32serviceutil.StartService(
-                Service._svc_name_
-            )
-            print('Start ok')
-        except Exception as x:
-            print(str(x))
-
-    def stop(self):
-        win32serviceutil.StopService(self.name)
-        # win32serviceutil.ServiceCtrlHandler(win32service.SERVICE_CONTROL_STOP)
+if __name__ == '__main__':
+    servicemanager.Initialize()
+    servicemanager.PrepareToHostSingle(Service)
+    servicemanager.StartServiceCtrlDispatcher()

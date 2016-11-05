5ca2e53ee74c45bcd1657a57969684c67a94b623
kasramvd <kasraavand@gmail.com>
2016-11-05 22:23:46 +0330

    update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-setup(name='mypackage',
-      packages=['mypackage'],  # this must be the same as the name above
+setup(name='FTPwalker',
+      packages=['FTPwalker'],
       version='0.1',
       description='A random test lib',
-      author='Peter Downs',
-      author_email='peterldowns@gmail.com',
-      url='https://github.com/peterldowns/mypackage',  # use the URL to the github repo
-      download_url='https://github.com/peterldowns/mypackage/tarball/0.1',  # I'll explain this in a second
-      keywords=['testing', 'logging', 'example'],  # arbitrary keywords
+      author='Bohdan Khomtchouk and Kasra A. Vand',
+      author_email='khomtchoukmed@gmail.com, kasraavand@gmail.com',
+      url='https://github.com/Bohdan-Khomtchouk/FTPwalker',
+      download_url='https://github.com/Bohdan-Khomtchouk/FTPwalker/tarball/0.1',
+      keywords=['FTP', 'traverse', 'directory', 'tree'],
       classifiers=[],)

f21c360094367ccc006b330e6e40ec111551f605
kasramvd <kasraavand@gmail.com>
2016-11-05 22:37:39 +0330

    Update the structure

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(name='FTPwalker',
       packages=['FTPwalker'],
       version='0.1',
-      description='A random test lib',
+      description='Optimally traversing extremely large FTP directory trees',
       author='Bohdan Khomtchouk and Kasra A. Vand',
       author_email='khomtchoukmed@gmail.com, kasraavand@gmail.com',
       url='https://github.com/Bohdan-Khomtchouk/FTPwalker',

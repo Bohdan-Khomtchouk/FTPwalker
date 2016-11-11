7315fe41814fa9306a643f43d712a41e0d3c9d35
kasramvd <kasraavand@gmail.com>
2016-11-11 19:18:35 +0330

    Update setup files

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+
+with open("description.rst") as f:
+    long_description = f.read()
+
 setup(name='FTPwalker',
       packages=['FTPwalker'],
       version='0.1',
-      description='Optimally traversing extremely large FTP directory trees',
+      description='Optimally traversing extremely large FTP directory trees.',
       author='Bohdan Khomtchouk and Kasra A. Vand',
       author_email='khomtchoukmed@gmail.com, kasraavand@gmail.com',
       url='https://github.com/Bohdan-Khomtchouk/FTPwalker',
       download_url='https://github.com/Bohdan-Khomtchouk/FTPwalker/tarball/0.1',
-      keywords=['FTP', 'traverse', 'directory', 'tree'],
-      classifiers=[],)
+      keywords=['FTP', 'traverse', 'directory tree'],
+      classifiers=[],
+      long_description=long_description)

1a2910997e5e2eb1c3d17f72e34a47d6fec2eaeb
kasramvd <kasraavand@gmail.com>
2017-02-24 16:00:50 +0330

    update setup, create new dist and minor improvements

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
 
-with open("description.rst") as f:
-    long_description = f.read()
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='FTPwalker',
-      packages=['FTPwalker'],
-      version='0.1',
+      packages=['FTPwalker', "FTPwalker/daemons"],
+      version='0.3',
       description='Optimally traversing extremely large FTP directory trees.',
-      author='Bohdan Khomtchouk and Kasra A. Vand',
+      author='Bohdan Khomtchouk and Kasra Vand',
       author_email='khomtchoukmed@gmail.com, kasraavand@gmail.com',
       url='https://github.com/Bohdan-Khomtchouk/FTPwalker',
       download_url='https://github.com/Bohdan-Khomtchouk/FTPwalker/tarball/0.1',
-      keywords=['FTP', 'traverse', 'directory tree'],
+      keywords=['FTP', 'traverse', 'directory tree', 'optimized'],
       classifiers=[],
-      long_description=long_description)
+      long_description=read("README"))
